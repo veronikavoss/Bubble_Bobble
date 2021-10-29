a8e7b164dbb889daf3209adf37eb2d3bcc51e611
SinD:/Users/Administrator/AppData/Local/Programs/Python/Python38/python.exe <veronikavoss.none@gmail.com>
2021-10-29 15:28:40 +0900

    3 animation

--- player.py
+++ player.py
@@ -5,34 +5,38 @@ class Player(pygame.sprite.Sprite):
     def __init__(self,element,x,y,tiles):
         super().__init__()
         self.tiles=tiles
-        
+        self.element=element
         self.jumped=False
         self.collide_left=False
         self.collide_right=False
         self.collide_ceiling=False
+        self.flip=True
         
         self.action='standby'
         self.index=0
-        self.image=element.player_images[self.action][self.index]
-        self.image.set_colorkey((15,79,174))
-        self.image=pygame.transform.flip(self.image,True,False)
+        self.image=self.element.player_images[self.action][self.index]
         self.rect=self.image.get_rect(x=x*2,y=y*2-32)
-        self.dx,self.dy=0,0
+        self.direction=pygame.math.Vector2(0,0)
+        self.dx,self.dy=self.direction.x,self.direction.y
         self.speed=6
         self.jump_speed=-14
         self.gravity=0.6
+        
+        self.animation_speed=0.1
     
     def key_input(self):
         key_pressed=pygame.key.get_pressed()
         
         if key_pressed[pygame.K_LEFT] and not self.collide_ceiling:
+            self.flip=False
             self.dx=-self.speed
         elif key_pressed[pygame.K_RIGHT] and not self.collide_ceiling:
+            self.flip=True
             self.dx=self.speed
         else:
             self.dx=0
         
-        if key_pressed[pygame.K_UP] and not self.jumped:
+        if key_pressed[pygame.K_UP] and not self.jumped and not self.action=='fall':
             self.dy=self.jump_speed
             self.jumped=True
         
@@ -50,6 +54,8 @@ class Player(pygame.sprite.Sprite):
         
         self.dy+=self.gravity
         self.rect.y+=self.dy
+        if self.dy>6:
+            self.dy=6
         
         for tile in self.tiles:
             if pygame.sprite.collide_mask(self,tile):
@@ -59,7 +65,6 @@ class Player(pygame.sprite.Sprite):
                     self.jumped=False
                 elif self.rect.top<=tile.rect.bottom and self.dy!=0:
                     self.collide_ceiling=True
-                    self.action='ceiling'
         
         if self.collide_left and self.dx>=0:
             self.collide_left=False
@@ -69,20 +74,35 @@ class Player(pygame.sprite.Sprite):
             self.collide_ceiling=False
     
     def set_action(self):
-        if self.dx<0:
-            self.action='left'
-        elif self.dx>0:
-            self.action='right'
-        elif self.dy<0:
+        if self.dy<0:
             self.action='jump'
         elif self.dy>1:
             self.action='fall'
+        elif self.dx!=0:
+            self.action='run'
+            self.animation_speed=0.05
         else:
             self.action='standby'
+            self.animation_speed=0.04
+    
+    def animation(self):
+        animation=self.element.player_images[self.action]
+        self.index+=self.animation_speed
+        if self.index>=len(animation):
+            self.index=0
+        self.image=animation[int(self.index)]
+        
+        if self.flip:
+            self.image=pygame.transform.flip(self.image,True,False)
+        else:
+            self.image=pygame.transform.flip(self.image,False,False)
+        
+        self.image.set_colorkey((15,79,174))
     
     def update(self):
         self.key_input()
         self.collision()
         self.set_action()
+        self.animation()
         
         print(self.action,self.collide_left,self.collide_right,self.collide_ceiling,self.dy)
